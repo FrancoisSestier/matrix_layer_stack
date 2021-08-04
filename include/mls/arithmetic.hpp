bd90beec66f140feab95a1f6fff906eafbcda730
Francois Sestier <francois.sestier@gmail.com>
2021-08-04 14:14:34 +0200

    fix: strong type operators were not working because underlying_type was used instead of acutal type
    added test for those comparaison, assaignement and equality operators

--- include/mls/arithmetic.hpp
+++ include/mls/arithmetic.hpp
@@ -1,162 +1,163 @@
 namespace mls {
-    
+
     template <typename T, typename tag>
     class arithmetic_type {
        public:
         using underlying_type = T;
+        using type =arithmetic_type<underlying_type,tag>;
         arithmetic_type() = default;
 
-        constexpr arithmetic_type(T const& value) noexcept(
-            std::is_nothrow_copy_constructible<T>::value)
+        constexpr arithmetic_type(const underlying_type & value) noexcept(
+            std::is_nothrow_copy_constructible<underlying_type>::value)
             : value_(value) {}
 
-        constexpr arithmetic_type(T&& value) noexcept(
-            std::is_nothrow_move_constructible<T>::value)
+        constexpr arithmetic_type(underlying_type&& value) noexcept(
+            std::is_nothrow_move_constructible<underlying_type>::value)
             : value_(std::move(value)) {}
 
-        explicit operator T&() noexcept { return value_; }
+        explicit operator underlying_type&() noexcept { return value_; }
 
-        explicit operator const T&() const noexcept { return value_; }
+        explicit operator const underlying_type&() const noexcept { return value_; }
 
         // equality
-        bool operator!=(const T& other) const {
-            return static_cast<underlying_type>(value_)
+        bool operator!=(const type& other) const {
+            return value_
                    != static_cast<underlying_type>(other);
         }
 
-        bool operator==(const T& other) const {
-            return static_cast<underlying_type>(value_)
+        bool operator==(const type& other) const {
+            return value_
                    == static_cast<underlying_type>(other);
         }
 
         // unary
-        [[nodiscard]] constexpr T operator+() const {
-            return +static_cast<underlying_type>(value_);
+        [[nodiscard]] constexpr type operator+() const {
+            return +value_;
         }
-        [[nodiscard]] constexpr T operator-() const {
-            return -static_cast<underlying_type>(value_);
+        [[nodiscard]] constexpr type operator-() const {
+            return -value_;
         }
 
         // addition
 
-        [[nodiscard]] T operator+(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator+(const type& other) noexcept {
+            return value_
                    + static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T& operator+=(T& other) noexcept {
+        [[nodiscard]] type& operator+=(type& other) noexcept {
             return value_ += other;
         }
 
         // substraction
 
-        [[nodiscard]] T operator-(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator-(const type& other) noexcept {
+            return value_
                    - static_cast<underlying_type>(other);
         };
 
-        [[nodiscard]] T& operator-=(T& other) noexcept {
+        [[nodiscard]] type& operator-=(type& other) noexcept {
             return value_ -= other;
         }
 
         // multiplication
 
-        [[nodiscard]] T operator*(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator*(const type& other) noexcept {
+            return value_
                    * static_cast<underlying_type>(other);
         };
 
-        [[nodiscard]] T& operator*=(T& other) noexcept {
+        [[nodiscard]] type& operator*=(type& other) noexcept {
             return value_ *= other;
         }
 
         // division
-        [[nodiscard]] T operator/(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator/(const type& other) noexcept {
+            return value_
                    * static_cast<underlying_type>(other);
         };
 
-        [[nodiscard]] T& operator/=(T& other) noexcept {
+        [[nodiscard]] type& operator/=(type& other) noexcept {
             return value_ /= other;
         }
 
         // decrementation
-        [[nodiscard]] T& operator--() noexcept {
-            --static_cast<underlying_type>(value_);
-            return value_;
+        [[nodiscard]] type& operator--() noexcept {
+            --value_;
+            return *this;
         };
 
-        [[nodiscard]] T& operator--(int) noexcept {
-            auto tmp = static_cast<underlying_type>(value_)--;
-            return tmp;
+        [[nodiscard]] type& operator--(int) noexcept {
+            auto tmp = value_--;
+            return *this;
         }
 
         // incrementation
-        [[nodiscard]] T& operator++() noexcept {
-            ++static_cast<underlying_type>(value_);
-            return value_;
+        [[nodiscard]] type& operator++() noexcept {
+            ++value_;
+            return *this;
         };
 
-        [[nodiscard]] T& operator++(int) noexcept {
-            auto tmp = static_cast<underlying_type>(value_)++;
+        [[nodiscard]] type& operator++(int) noexcept {
+            auto tmp = value_++;
             return tmp;
         }
 
         // modulo
-        [[nodiscard]] T operator%(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator%(const type& other) noexcept {
+            return value_
                    % static_cast<underlying_type>(other);
         };
 
         // bitwise
-        [[nodiscard]] T operator~() noexcept {
-            return ~static_cast<underlying_type>(value_);
+        [[nodiscard]] type operator~() noexcept {
+            return ~value_;
         }
 
-        [[nodiscard]] T operator&(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator&(const type& other) noexcept {
+            return value_
                    & static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T operator|(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] underlying_type operator|(const type& other) noexcept {
+            return value_
                    | static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T operator^(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator^(const type& other) noexcept {
+            return value_
                    ^ static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T operator<<(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator<<(const type& other) noexcept {
+            return value_
                    << static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T operator>>(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        [[nodiscard]] type operator>>(const type& other) noexcept {
+            return value_
                    >> static_cast<underlying_type>(other);
         }
 
         // compare
 
-        [[nodiscard]] T operator>=(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        bool operator>=(const type& other) noexcept {
+            return value_
                    >= static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T operator>(const T& other) noexcept {
-                       return static_cast<underlying_type>(value_)
+        bool operator>(const type& other) noexcept {
+            return value_
                    > static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T operator<=(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        bool operator<=(const type& other) noexcept {
+            return value_
                    <= static_cast<underlying_type>(other);
         }
 
-        [[nodiscard]] T operator<(const T& other) noexcept {
-            return static_cast<underlying_type>(value_)
+        bool operator<(const type& other) noexcept {
+            return value_
                    < static_cast<underlying_type>(other);
         }
 
--- test/src/types/types.cpp
+++ test/src/types/types.cpp
@@ -1,21 +1,47 @@
 #include <gtest/gtest.h>
+
 #include <mls/strong_types.hpp>
 
 using namespace mls;
 
-    using elevation = s_int16_t<struct elevation_tag>;
-    using tile_id = s_int32_t<struct tile_id_tag>;
+using elevation = s_int16_t<struct elevation_tag>;
+using tile_id = s_int32_t<struct tile_id_tag>;
 
-TEST(MLS,INT_TYPES) {
-    ASSERT_NE(typeid(elevation).hash_code(),typeid(tile_id).hash_code());
+TEST(MLS, INT_TYPES) {
+    ASSERT_NE(typeid(elevation).hash_code(), typeid(tile_id).hash_code());
 
     elevation el(51);
-    
+
     tile_id tid(5);
 };
 
-TEST(MLS,SIZE_OF){
-    ASSERT_EQ(sizeof(elevation),sizeof(int16_t));
-    ASSERT_EQ(sizeof(tile_id),sizeof(int32_t));
+TEST(MLS, SIZE_OF) {
+    ASSERT_EQ(sizeof(elevation), sizeof(int16_t));
+    ASSERT_EQ(sizeof(tile_id), sizeof(int32_t));
+}
 
+TEST(MLS, ASSIGNMENT) {
+    elevation el = 51;
+    elevation& el2 = el;
+    elevation el3 = el;
+}
+
+TEST(MLS, COMPARE) {
+    elevation el = 0;
+    elevation el2 = 10;
+    ASSERT_TRUE(el < el2);
+    ASSERT_TRUE(el < 15);
+    ASSERT_TRUE(el <= el2);
+    ASSERT_TRUE(el >= 0);
+    ASSERT_TRUE(el2 > 9);
+    ASSERT_TRUE(el2 > elevation(5));
+    ASSERT_EQ(el, 0);
+    ASSERT_NE(el, 10);
+}
+
+TEST(MLS, CAST) {
+    int i = -5;
+    elevation el2 = static_cast<elevation>(i);
+    ASSERT_EQ(el2, -5);
+    elevation::underlying_type i2 = static_cast<elevation::underlying_type>(el2);
 }
